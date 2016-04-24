5a894646b792db29cd12655a5b32f62ea0daec48
Julien Gamba <jgamba@aius.u-strasbg.fr>
2016-04-24 16:56:53 +0200

    Added host model

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424145325) do
+ActiveRecord::Schema.define(version: 20160424145620) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20160424145325) do
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+
+  create_table "hosts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "services", force: :cascade do |t|
     t.string   "status"
