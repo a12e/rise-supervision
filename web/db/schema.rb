308301490c13d53568ad900ca969a337493da444
Julien Gamba <jgamba@aius.u-strasbg.fr>
2016-04-24 17:20:01 +0200

    Forgot IDs for services and hosts

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424145620) do
+ActiveRecord::Schema.define(version: 20160424151944) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160424145620) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "host_id"
   end
 
   create_table "services", force: :cascade do |t|
@@ -40,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160424145620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.integer  "service_id"
   end
 
   create_table "users", force: :cascade do |t|
