baa45c5035c2eff87c8d52dbb18a4e2816d37eb7
Julien Gamba <jgamba@aius.u-strasbg.fr>
2016-04-24 16:54:15 +0200

    Added name to service and DB migration

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424105200) do
+ActiveRecord::Schema.define(version: 20160424145325) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160424105200) do
     t.text     "infos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
